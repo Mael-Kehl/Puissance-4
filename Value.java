e46224bd936c0b0356046a3c2dabd863388ef348
Mael-Kehl <artyworldyah@gmail.com>
2021-02-22 14:59:52 +0100

    javadoc

--- Value.java
+++ Value.java
@@ -1,9 +1,27 @@
+/**
+* Used to recover an input
+*/
 import java.util.Scanner;
+/**
+* Used to check for values errors
+*/
 import java.util.InputMismatchException;
 
+/**
+* Used to recover a user input and to verify if the value is correct
+* @see Game.java
+*/
 public class Value{
+  /**
+  * Variable of that will allow us to recover a value
+  */
   private static Scanner entry = new Scanner(System.in);
 
+  /**
+  * Allows us to recover a value, to check if it is an integer and if it isn't to print an erro message and ask user to retry
+  * @param ask is a String but we expect it to be an integer
+  * @return the value entered if it was an integer
+  */
   public static int Recup(String ask){
     int y;
     do {
@@ -18,6 +36,6 @@ public class Value{
     } while (true);
     --y;
     return y;
-}
+  }
 
 }
